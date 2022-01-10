d964ad0045ad5c2dd0baa018793687893fca305d
Pieter Vermeulen <pietermeulen@hotmail.com>
2022-01-10 10:18:32 +0100

    random backgroundcolor

--- script.js
+++ script.js
@@ -6,4 +6,5 @@ function random_rgba() {
     var o = Math.round, r = Math.random, s = 255;
     return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
 }
-document.body.style.backgroundColor= "#FF69B4"
+var color = random_rgba();
+document.body.style.backgroundColor= color

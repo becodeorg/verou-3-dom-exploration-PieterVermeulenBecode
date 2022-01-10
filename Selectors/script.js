31accd33e69978c8916ce30c6d913c3d0c68c861
Pieter Vermeulen <pietermeulen@hotmail.com>
2022-01-10 12:21:56 +0100

    images display: none

--- Selectors/script.js
+++ Selectors/script.js
@@ -5,3 +5,12 @@ for (const x of allImportants){
     console.log(x)
     x.setAttribute('title', "This is an important item");
 }
+
+
+const allImages= document.querySelectorAll("img")
+console.log(allImages);
+
+for (const x of allImages){
+    console.log(x)
+    x.style.display="none";
+}

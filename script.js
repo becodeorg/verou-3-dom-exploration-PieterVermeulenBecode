7cc4c4f47c46d2598dd2231b59e71049ab65c089
Pieter Vermeulen <pietermeulen@hotmail.com>
2022-01-10 10:13:01 +0100

    pink background

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log(document.title)
+document.title="new title"
+console.log(document.title)
+document.body.style.backgroundColor= "#FF69B4"
+function random_rgba() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+document.body.style.backgroundColor= "#FF69B4"

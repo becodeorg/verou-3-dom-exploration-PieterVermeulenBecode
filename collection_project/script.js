19f8b193f1e54d6b2b6b25f3ba4ef79b16e4dc1e
Pieter Vermeulen <pietermeulen@hotmail.com>
2022-01-14 10:50:21 +0100

    Primitive Search function added

--- collection_project/script.js
+++ collection_project/script.js
@@ -1,6 +1,19 @@
 
 document.body.onload = addElement(boardGames);
-
+function Search(){
+  const form= document.getElementById("Name")
+  const kaarten=document.getElementsByTagName("h2")
+  
+  for (let kaart of kaarten){
+    
+    if(kaart.textContent!=form.value){
+    let parent =kaart.parentElement;
+    console.log(parent);
+     parent.style.display="none";}
+  }
+  
+  
+}
 function addElement (items) {
     let currentDiv = document.getElementById("cardCollection");
     
@@ -8,29 +21,34 @@ function addElement (items) {
     const newCard = document.createElement("div");
     newCard.className="card"; //Create a card
 
+    const newImage = document.createElement("img"); //Create a Image
+    newImage.src =item.Picture;    //asign a value to the Image
+    newCard.appendChild(newImage); //add the Image to the card
+
+    
     const newTitle = document.createElement("h2"); //Create a Title
     newTitle.textContent=item.Title;    //asign a value to the Title
     newCard.appendChild(newTitle);      //add the title to the card
 
-    const newImage = document.createElement("img"); //Create a Image
-    newImage.src =item.Picture;    //asign a value to the Image
-    newCard.appendChild(newImage); //add the Image to the card
+    const newParagraph = document.createElement("p"); //Create a paragraph
+    newParagraph.textContent=item.Description;    //asign a value to the paragraph
+    newCard.appendChild(newParagraph); //add the paragraph to the card
 
     const newDesignerDiv = document.createElement("div"); //Create a div
     newDesignerDiv.className ="designer";    //asign a class to the div
     newCard.appendChild(newDesignerDiv); //add the Div to the card
 
-    const newParagraphScore = document.createElement("p"); //Create a Paragraph for the score
-    newParagraphScore.textContent=item.Score;    //asign a value to the Paragraph
-    newDesignerDiv.appendChild(newParagraphScore); //add the Paragraph to the designerdiv
-
     const newDesigner = document.createElement("p"); //Create a Paragraph for the Designer
     newDesigner.textContent=item.Designer;    //asign a value to the Paragraph
     newDesignerDiv.appendChild(newDesigner); //add the Paragraph to the designerdiv
     
-    const newParagraph = document.createElement("p"); //Create a paragraph
-    newParagraph.textContent=item.Description;    //asign a value to the paragraph
-    newCard.appendChild(newParagraph); //add the paragraph to the card
+    const newParagraphPlayerNr = document.createElement("p"); //Create a Paragraph for the PlayerNr
+    newParagraphPlayerNr.textContent=item.PlayerNr;    //asign a value to the Paragraph
+    newDesignerDiv.appendChild(newParagraphPlayerNr); //add the Paragraph to the designerdiv
+
+    const newParagraphScore = document.createElement("p"); //Create a Paragraph for the score
+    newParagraphScore.textContent=item.Score;    //asign a value to the Paragraph
+    newDesignerDiv.appendChild(newParagraphScore); //add the Paragraph to the designerdiv
 
     
 

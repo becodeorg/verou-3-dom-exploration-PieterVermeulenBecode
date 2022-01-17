bc71adee59b0ce56aac167f7d914dbeffd33d6b0
Pieter Vermeulen <pietermeulen@hotmail.com>
2022-01-17 14:32:26 +0100

    Switching headers of section

--- Navigate-the-dom-tree/script.js
+++ Navigate-the-dom-tree/script.js
@@ -2,3 +2,14 @@ const parent=document.getElementsByTagName("ol")[0];
 const lastChild=parent.lastElementChild;
 parent.prepend(lastChild);
 
+
+const secondSection=document.getElementsByTagName("section")[1];
+const thirdSection=document.getElementsByTagName("section")[2];
+const thirdTitle=thirdSection.firstElementChild;
+
+
+const h2to3=secondSection.firstElementChild;
+const h3to2=thirdSection.firstElementChild;
+
+secondSection.prepend(h3to2);
+thirdSection.prepend(h2to3);
